fd12c0ca11d614d16596521a2d6161fa432327d5
MrUnwonted <arjungodzarjun@gmail.com>
2023-08-17 12:01:32 +0530

    Role

--- src/main/java/com/arjun/demo/config/SecurityConfig.java
+++ src/main/java/com/arjun/demo/config/SecurityConfig.java
@@ -2,9 +2,12 @@ package com.arjun.demo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -23,4 +26,17 @@ public class SecurityConfig {
                 .build();
         return new InMemoryUserDetailsManager(user,admin);
     }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/admin").hasRole("ADMIN")
+                        .requestMatchers("/user").hasAnyRole("USER","ADMIN")
+                        .requestMatchers("/").permitAll()
+
+                        .and().formLogin().permitAll());
+        return http.build();
+    }
+
 }
--- src/main/java/com/arjun/demo/controller/HomeController.java
+++ src/main/java/com/arjun/demo/controller/HomeController.java
@@ -6,8 +6,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 
-    @GetMapping
+    @GetMapping("/")
     public String printMethod(){
         return "Welcome";
     }
+
+    @GetMapping("/user")
+    public String getUser(){
+        return "Welcome User";
+    }
+
+    @GetMapping("/admin")
+    public String getAdmin(){
+        return "Welcome Admin";
+    }
+
 }
